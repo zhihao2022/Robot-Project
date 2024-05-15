38436bbf094083d1e414738717d9a5c890780985
pku2100013176 <2100013176@stu.pku.edu.cn>
2024-05-15 16:30:54 +0800

    初始化驱动

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,99 @@
 #include <Arduino.h>
+#include <Servo.h>
+#include "constants.h"
 
-int LED = 13;//定义数字接口10,对应
+Servo hipL;
+Servo hipR;
+Servo kneeL;
+Servo kneeR;
+Servo ankleL;
+Servo ankleR;
+
+void updateServoPos(int target1, int target2, int target3, char leg){
+  if (leg == 'l'){
+    hipL.write(hipLOffset - target1);
+    kneeL.write(kneeLOffset - target2);
+    ankleL.write(2*ankleLOffset - target3);
+  }
+  else if (leg == 'r'){
+    hipR.write(hipROffset + target1);
+    kneeR.write(kneeROffset + target2);
+    ankleR.write(target3);
+  }
+}
+
+void pos(float x, float z, char leg){
+  float hipRad2 = atan(x/z);
+  float hipDeg2 = hipRad2 * (180/PI);
+
+  float z2 = z/cos(hipRad2);
+
+  float hipRad1 = acos((sq(l1) + sq(z2) - sq(l2))/(2*l1*z2));
+  float hipDeg1 = hipRad1 * (180/PI);
+  
+  float kneeRad = PI - acos((sq(l1) + sq(l2) - sq(z2))/(2*l1*l2));
+
+  float ankleRad = PI/2 + hipRad2 - acos((sq(l2) + sq(z2) - sq(l1))/(2*l2*z2));
+  
+  float hipDeg = hipDeg1 + hipDeg2;
+  float kneeDeg = kneeRad * (180/PI);
+  float ankleDeg = ankleRad * (180/PI);
+
+//  Serial.print(hipDeg);
+//  Serial.print("\t");
+//  Serial.print(kneeDeg);
+//  Serial.print("\t");
+//  Serial.println(ankleDeg);
+
+  updateServoPos(hipDeg, kneeDeg, ankleDeg, leg);  
+}
+
+void takeStep(float stepLength, int stepVelocity){
+  for (float i = stepLength; i >= -stepLength; i-=0.5){
+    pos(i, stepHeight, 'r');
+    pos(-i, stepHeight - stepClearance, 'l');
+    delay(stepVelocity);
+  }
+
+  for (float i = stepLength; i >= -stepLength; i-=0.5){
+    pos(-i, stepHeight - stepClearance, 'r');
+    pos(i, stepHeight, 'l');
+    delay(stepVelocity);
+  }
+}
+
+void initialize(){
+  for (float i = 10.7; i >= stepHeight; i-=0.1){
+    pos(0, i, 'l');
+    pos(0, i, 'r');
+  }
+}
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LED,OUTPUT);//设置数字10 口为输出接口
+  Serial.begin(9600);
+  hipL.attach(9);
+  hipR.attach(8);
+  kneeL.attach(7);
+  kneeR.attach(6);
+  ankleL.attach(5);
+  ankleR.attach(4);
+
+  hipL.write(hipLOffset);
+  kneeL.write(kneeLOffset);
+  ankleL.write(ankleLOffset);
+  
+  hipR.write(hipROffset);
+  kneeR.write(kneeROffset);
+  ankleR.write(ankleROffset);
+
+  delay(5000);
+  
+  initialize();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(LED,HIGH);//IO 口10 设置为高电平，点亮数字10 口LED
-
-  delay(500);//延时500MS
-
-  digitalWrite(LED,LOW);//IO 口10 设置为低电平，熄灭数字10 口LED
-
-  delay(500);//延时500MS
+  takeStep(2, 0);
 }
+
